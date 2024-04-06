5488eaf30e217b5a9f3ec252cc64df460dfa53b4
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-06 13:04:58 +0500

    -Result State Impl (#20)

---
+++ composeApp/src/commonMain/kotlin/org/flexi/app/presentation/viewmodels/MainViewModel.kt
@@ -0,0 +1,29 @@
+package org.flexi.app.presentation.viewmodels
+
+import dev.icerock.moko.mvvm.viewmodel.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import org.flexi.app.domain.repository.Repository
+import org.flexi.app.domain.usecase.ResultState
+
+class MainViewModel(
+    private val repository: Repository,
+) : ViewModel() {
+
+    private val _login = MutableStateFlow<ResultState<String>>(ResultState.Loading)
+    val login: StateFlow<ResultState<String>> = _login.asStateFlow()
+
+    fun loginUser(email: String, password: String) {
+        viewModelScope.launch {
+            try {
+                val response = repository.loginUser(email, password)
+                _login.value = ResultState.Success(response)
+            } catch (e: Exception) {
+                _login.value = ResultState.Error(e)
+            }
+        }
+    }
+
+}
