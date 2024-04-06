1398b5264534114c2de337f9de5f41c2db49dd50
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-06 15:49:20 +0500

    -UI Enhancement (#22)

--- composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/HomeScreen.kt
@@ -60,68 +60,7 @@ class HomeScreen: Screen {
                 .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = stringResource(Res.string.cyclone),
-                fontFamily = FontFamily(Font(Res.font.IndieFlower_Regular)),
-                style = MaterialTheme.typography.displayLarge
-            )
 
-            var isAnimate by remember { mutableStateOf(false) }
-            val transition = rememberInfiniteTransition()
-            val rotate by transition.animateFloat(
-                initialValue = 0f,
-                targetValue = 360f,
-                animationSpec = infiniteRepeatable(
-                    animation = tween(1000, easing = LinearEasing)
-                )
-            )
-
-            Image(
-                modifier = Modifier
-                    .size(250.dp)
-                    .padding(16.dp)
-                    .run { if (isAnimate) rotate(rotate) else this },
-                imageVector = vectorResource(Res.drawable.ic_cyclone),
-                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
-                contentDescription = null
-            )
-
-            ElevatedButton(
-                modifier = Modifier
-                    .padding(horizontal = 8.dp, vertical = 4.dp)
-                    .widthIn(min = 200.dp),
-                onClick = { isAnimate = !isAnimate },
-                content = {
-                    Icon(vectorResource(Res.drawable.ic_rotate_right), contentDescription = null)
-                    Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-                    Text(
-                        stringResource(if (isAnimate) Res.string.stop else Res.string.run)
-                    )
-                }
-            )
-
-            var isDark by LocalThemeIsDark.current
-            val icon = remember(isDark) {
-                if (isDark) Res.drawable.ic_light_mode
-                else Res.drawable.ic_dark_mode
-            }
-
-            ElevatedButton(
-                modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp).widthIn(min = 200.dp),
-                onClick = { isDark = !isDark },
-                content = {
-                    Icon(vectorResource(icon), contentDescription = null)
-                    Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-                    Text(stringResource(Res.string.theme))
-                }
-            )
-
-            TextButton(
-                modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp).widthIn(min = 200.dp),
-                onClick = { openUrl("https://github.com/terrakok") },
-            ) {
-                Text(stringResource(Res.string.open_github))
-            }
         }
     }
 }
