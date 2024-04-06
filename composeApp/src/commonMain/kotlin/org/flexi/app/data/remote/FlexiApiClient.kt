266ca7e5aa61fc3563510b5ae80174ab04778c07
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-06 12:32:54 +0500

    -Request TimeOut Impl (#10)

---
+++ composeApp/src/commonMain/kotlin/org/flexi/app/data/remote/FlexiApiClient.kt
@@ -0,0 +1,37 @@
+package org.flexi.app.data.remote
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import org.flexi.app.utils.Constant.TIME_OUT
+
+object FlexiApiClient {
+    private val client = HttpClient {
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                }
+            )
+        }
+        install(Logging) {
+            level = LogLevel.ALL
+            logger = object : Logger {
+                override fun log(message: String) {
+                    println(message)
+                }
+            }
+        }
+        install(HttpTimeout){
+            socketTimeoutMillis = TIME_OUT
+            connectTimeoutMillis = TIME_OUT
+            requestTimeoutMillis = TIME_OUT
+        }
+    }
+}
--- composeApp/src/commonMain/kotlin/org/flexi/app/utils/Constant.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/utils/Constant.kt
@@ -2,4 +2,5 @@ package org.flexi.app.utils
 
 object Constant {
     const val BASE_URL = "http://127.0.0.1:8080/"
+    const val TIME_OUT = 300000L
 }
