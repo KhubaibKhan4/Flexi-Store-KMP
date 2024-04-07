babccf296d189823703bf2f6bd63e5b7bcf0bfa1
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-07 10:33:10 +0500

    -Login Screen Impl (#34)

---
+++ composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/auth/login/LoginScreen.kt
@@ -0,0 +1,130 @@
+package org.flexi.app.presentation.ui.screens.auth.login
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Facebook
+import androidx.compose.material.icons.filled.Synagogue
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import cafe.adriel.voyager.core.screen.Screen
+import org.flexi.app.presentation.ui.components.CustomTextField
+import org.flexi.app.presentation.ui.components.HeadlineText
+
+class LoginScreen : Screen {
+    @Composable
+    override fun Content() {
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        var passwordVisible by remember { mutableStateOf(false) }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Center
+        ) {
+            HeadlineText(text = "Login Account")
+            Text(
+                text = "Please login with registered account",
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(14.dp))
+            CustomTextField(
+                input = email,
+                label = "Email",
+                onValueChange = { email = it },
+                isError = false,
+                leadingIcon = Icons.Outlined.Email,
+                isPasswordVisible = true
+            )
+            Spacer(modifier = Modifier.height(6.dp))
+            CustomTextField(
+                input = password,
+                label = "Password",
+                onValueChange = { password = it },
+                isError = false,
+                leadingIcon = Icons.Outlined.Lock,
+                showPasswordToggle = true,
+                isPasswordVisible = passwordVisible,
+                onPasswordToggleClick = { passwordVisible = !passwordVisible }
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                TextButton(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Blue,
+                        contentColor = Color.White
+                    ),
+                    modifier = Modifier.fillMaxWidth()
+                        .clip(RoundedCornerShape(16.dp))
+                        .padding(start = 8.dp, end = 8.dp, top = 8.dp)
+                ) {
+                    Text("Sign In")
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    "or using other method",
+                    fontSize = 10.sp,
+                    color = Color.Gray
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    IconButton(
+                        onClick = { /* Handle Google signup */ },
+                        modifier = Modifier.size(48.dp)
+                    ) {
+                        Icon(
+                            Icons.Default.Synagogue,
+                            contentDescription = null
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(16.dp))
+                    IconButton(
+                        onClick = { /* Handle Facebook signup */ },
+                        modifier = Modifier.size(48.dp)
+                    ) {
+                        Icon(
+                            Icons.Default.Facebook,
+                            contentDescription = null
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
