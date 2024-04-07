77ef555937f7447f80800f90cb88763e810bc336
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-07 10:48:35 +0500

    -Js Injection (#40)

--- composeApp/src/jsMain/kotlin/main.kt
+++ composeApp/src/jsMain/kotlin/main.kt
@@ -1,10 +1,15 @@
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
 import org.flexi.app.App
+import org.flexi.app.di.appModule
 import org.jetbrains.skiko.wasm.onWasmReady
+import org.koin.core.context.startKoin
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    startKoin {
+        modules(appModule)
+    }
     onWasmReady {
         CanvasBasedWindow("Flexi-Store") {
             App()
