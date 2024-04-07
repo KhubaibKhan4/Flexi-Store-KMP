ea57b8e526c750a3b298d68cbdc4be26e6c59bab
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-07 11:08:53 +0500

    -Singleton Impl (#46)

--- composeApp/src/commonMain/kotlin/org/flexi/app/domain/repository/Repository.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/domain/repository/Repository.kt
@@ -2,13 +2,11 @@ package org.flexi.app.domain.repository
 
 import org.flexi.app.data.remote.FlexiApiClient
 import org.flexi.app.data.repository.FlexiApi
-import org.koin.core.annotation.Module
 import org.koin.core.annotation.Single
 
-@Module
-class Repository: FlexiApi {
-    @Single
+@Single
+class Repository : FlexiApi {
     override suspend fun loginUser(email: String, password: String): String {
-      return FlexiApiClient.loginUser(email, password)
+        return FlexiApiClient.loginUser(email, password)
     }
 }
