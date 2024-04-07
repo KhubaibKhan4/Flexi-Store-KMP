432203f1d326342111be2a2b93ba11d6a8463000
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-07 10:50:33 +0500

    -Koin Injection Impl (#42)

--- composeApp/src/iosMain/kotlin/main.kt
+++ composeApp/src/iosMain/kotlin/main.kt
@@ -1,5 +1,12 @@
 import androidx.compose.ui.window.ComposeUIViewController
 import org.flexi.app.App
+import org.flexi.app.di.appModule
+import org.koin.core.context.startKoin
 import platform.UIKit.UIViewController
 
 fun MainViewController(): UIViewController = ComposeUIViewController { App() }
+fun initKoin(){
+    startKoin {
+        modules(appModule)
+    }
+}
