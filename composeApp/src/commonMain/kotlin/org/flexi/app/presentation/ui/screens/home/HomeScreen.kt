56c15b8e3aa16fd00fdffefc2396f7f74e48b004
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-07 15:56:11 +0500

    -Products List Impl (#75)

--- composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/home/HomeScreen.kt
@@ -9,17 +9,50 @@ import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
+import org.flexi.app.domain.model.products.Products
+import org.flexi.app.domain.usecase.ResultState
+import org.flexi.app.presentation.ui.components.ErrorBox
+import org.flexi.app.presentation.ui.components.LoadingBox
 import org.flexi.app.presentation.ui.screens.auth.signup.SignupScreen
+import org.flexi.app.presentation.viewmodels.MainViewModel
+import org.koin.compose.koinInject
 
 class HomeScreen : Screen {
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.current
+        var productsList by remember { mutableStateOf<List<Products>?>(null) }
+        val viewModel: MainViewModel = koinInject<MainViewModel>()
+        LaunchedEffect(Unit) {
+            viewModel.getProducts()
+        }
+        val state by viewModel.products.collectAsState()
+        when (state) {
+            is ResultState.Error -> {
+                val error = (state as ResultState.Error).error
+                ErrorBox(error)
+            }
+
+            is ResultState.Loading -> {
+                LoadingBox()
+            }
+
+            is ResultState.Success -> {
+                val response = (state as ResultState.Success).response
+                productsList = response
+            }
+        }
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -30,7 +63,8 @@ class HomeScreen : Screen {
             Text("Signup",
                 modifier = Modifier.clickable {
                     navigator?.push(SignupScreen())
-                })
+                }
+            )
         }
     }
 }
