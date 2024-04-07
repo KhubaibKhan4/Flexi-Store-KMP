cfc1ed027589147a2c0c9bf4a8f3913d29f0e1fe
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-07 15:50:15 +0500

    -Get Products Enhancement (#73)

--- composeApp/src/commonMain/kotlin/org/flexi/app/presentation/viewmodels/MainViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/presentation/viewmodels/MainViewModel.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import org.flexi.app.domain.model.products.Products
 import org.flexi.app.domain.repository.Repository
 import org.flexi.app.domain.usecase.ResultState
 import org.koin.android.annotation.KoinViewModel
@@ -20,6 +21,8 @@ class MainViewModel(
     private val _signup = MutableStateFlow<ResultState<String>>(ResultState.Loading)
     val signup = _signup.asStateFlow()
 
+    private val _products = MutableStateFlow<ResultState<List<Products>>>(ResultState.Loading)
+    val products: StateFlow<ResultState<List<Products>>> = _products.asStateFlow()
     fun loginUser(email: String, password: String) {
         viewModelScope.launch {
             try {
@@ -30,13 +33,26 @@ class MainViewModel(
             }
         }
     }
-    fun signupUser(username: String,email: String, password: String) {
+
+    fun signupUser(username: String, email: String, password: String) {
         viewModelScope.launch {
             try {
                 val response = repository.signupUser(username, email, password)
                 _signup.value = ResultState.Success(response)
             } catch (e: Exception) {
                 _signup.value = ResultState.Error(e)
+            }
+        }
+    }
+
+    fun getProducts() {
+        viewModelScope.launch {
+            _products.value = ResultState.Loading
+            try {
+                val response = repository.getProducts()
+                _products.value = ResultState.Success(response)
+            } catch (e: Exception) {
+                _products.value = ResultState.Error(e)
             }
         }
     }
