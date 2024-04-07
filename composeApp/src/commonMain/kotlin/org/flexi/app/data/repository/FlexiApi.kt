d309d925ad8eca9f0c76e176058e697eb61af2fc
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-07 11:10:05 +0500

    -FlexiApi Singleton Impl (#47)

--- composeApp/src/commonMain/kotlin/org/flexi/app/data/repository/FlexiApi.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/data/repository/FlexiApi.kt
@@ -1,5 +1,8 @@
 package org.flexi.app.data.repository
 
+import org.koin.core.annotation.Single
+
+@Single
 interface FlexiApi {
     suspend fun loginUser(email: String, password: String): String
 }
