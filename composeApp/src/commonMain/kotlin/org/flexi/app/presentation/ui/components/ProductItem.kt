aaac08f3fb101f64809376d48ccfef85b20ea212
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-08 16:06:35 +0500

    -Product Items UI Impl (#113)

--- composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/components/ProductItem.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/components/ProductItem.kt
@@ -1,27 +1,90 @@
 package org.flexi.app.presentation.ui.components
 
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import io.kamel.core.Resource
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
 import org.flexi.app.domain.model.products.Products
-import org.jetbrains.compose.resources.Resource
+import org.flexi.app.utils.Constant.BASE_URL
 
 @Composable
 fun ProductItem(
     products: Products,
 ) {
+    val image: Resource<Painter> = asyncPainterResource(data = BASE_URL + products.imageUrl)
     Column(
         modifier = Modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Box(modifier = Modifier.fillMaxWidth()){
-
+        Box(modifier = Modifier.fillMaxWidth()) {
+            KamelImage(
+                resource = image,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(250.dp)
+                    .clip(RoundedCornerShape(14.dp)),
+                contentScale = ContentScale.Crop,
+                animationSpec = tween(),
+            )
+            Icon(
+                imageVector = Icons.Default.Favorite,
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier.size(55.dp)
+                    .clip(CircleShape)
+                    .background(Color.White.copy(alpha = 0.65f))
+            )
+        }
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = products.name,
+                fontSize = MaterialTheme.typography.titleMedium.fontSize,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                color = Color.Black
+            )
+            Text(
+                text = products.categoryTitle,
+                fontSize = MaterialTheme.typography.bodySmall.fontSize,
+                fontWeight = FontWeight.Normal,
+                textAlign = TextAlign.Center,
+            )
+            Text(
+                text = "$" + products.price,
+                fontSize = MaterialTheme.typography.titleSmall.fontSize,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+            )
         }
     }
 }
