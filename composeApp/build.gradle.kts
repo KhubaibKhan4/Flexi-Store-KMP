22a3c87e2b97c103d18f70813b389b3900089fee
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-08 13:17:30 +0500

    -Bottom Navigation Impl (#105)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -60,6 +60,7 @@ kotlin {
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material3)
+            implementation(compose.material)
             implementation(compose.materialIconsExtended)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
--- composeApp/src/commonMain/kotlin/org/flexi/app/App.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/App.kt
@@ -1,5 +1,19 @@
 package org.flexi.app
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.BottomNavigation
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DeliveryDining
 import androidx.compose.material.icons.filled.Favorite
@@ -9,18 +23,48 @@ import androidx.compose.material.icons.outlined.DeliveryDining
 import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.navigator.Navigator
-import org.flexi.app.presentation.ui.screens.auth.login.LoginScreen
+import cafe.adriel.voyager.navigator.tab.CurrentTab
+import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabNavigator
 import org.flexi.app.presentation.ui.screens.navigation.rails.items.NavigationItem
-import org.flexi.app.presentation.ui.screens.navigation.rails.navbar.NavigationIcon
+import org.flexi.app.presentation.ui.screens.navigation.rails.navbar.NavigationSideBar
+import org.flexi.app.presentation.ui.screens.navigation.tabs.favourite.FavouriteTab
+import org.flexi.app.presentation.ui.screens.navigation.tabs.home.HomeTab
+import org.flexi.app.presentation.ui.screens.navigation.tabs.orders.MyOrders
+import org.flexi.app.presentation.ui.screens.navigation.tabs.profile.ProfileTab
 import org.flexi.app.theme.AppTheme
+import org.flexi.app.theme.LocalThemeIsDark
 
 @Composable
 internal fun App() = AppTheme {
-    Navigator(LoginScreen())
+    AppContent()
 }
 
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 @Composable
 fun AppContent() {
     val items = listOf(
@@ -50,7 +94,134 @@ fun AppContent() {
             hasNews = false,
         ),
     )
+    val windowClass = calculateWindowSizeClass()
+    val showNavigationRail = windowClass.widthSizeClass != WindowWidthSizeClass.Compact
+    var selectedItemIndex by rememberSaveable {
+        mutableStateOf(0)
+    }
+    TabNavigator(HomeTab) { tabNavigator ->
+        Scaffold(modifier = Modifier.fillMaxWidth(),
+            bottomBar = {
+            if (!showNavigationRail) {
+                BottomNavigation(
+                    modifier = Modifier.fillMaxWidth().windowInsetsPadding(WindowInsets.ime),
+                    backgroundColor = MaterialTheme.colorScheme.background,
+                    contentColor = contentColorFor(Color.Red),
+                    elevation = 8.dp
+                ) {
+                    TabItem(HomeTab)
+                    TabItem(MyOrders)
+                    TabItem(FavouriteTab)
+                    TabItem(ProfileTab)
+                }
+            }
+        }) {
+            Column(
+                modifier = Modifier.fillMaxSize().padding(
+                    bottom = it.calculateBottomPadding(),
+                    start = if (showNavigationRail) 80.dp else 0.dp
+                )
+            ) {
+                CurrentTab()
+            }
+        }
+    }
+    if (showNavigationRail) {
+        NavigationSideBar(
+            items = items,
+            selectedItemIndex = selectedItemIndex,
+            onNavigate = {
+                selectedItemIndex = it
+            }
+        )
 
+        Box(
+            modifier = Modifier.fillMaxSize()
+                .padding(start = 80.dp)
+        ) {
+            when (selectedItemIndex) {
+                0 -> {
+
+                }
+
+                1 -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.background)
+                    ) {
+                        TabNavigator(MyOrders)
+                    }
+                }
+
+                2 -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.background)
+                    ) {
+                        TabNavigator(FavouriteTab)
+                    }
+                }
+
+                3 -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.background)
+                    ) {
+                        TabNavigator(ProfileTab)
+                    }
+                }
+            }
+        }
+
+    }
+}
+
+@Composable
+fun RowScope.TabItem(tab: Tab) {
+    val isDark by LocalThemeIsDark.current
+    val tabNavigator = LocalTabNavigator.current
+    NavigationBarItem(
+        modifier = Modifier.fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surface)
+            .height(58.dp).clip(RoundedCornerShape(16.dp)),
+        selected = tabNavigator.current == tab,
+        onClick = {
+            tabNavigator.current = tab
+        },
+        icon = {
+            tab.options.icon?.let { painter ->
+                Icon(
+                    painter,
+                    contentDescription = tab.options.title,
+                    tint = if (tabNavigator.current == tab) Color.Red else if (isDark) Color.White else Color.Black
+                )
+            }
+        },
+        label = {
+            tab.options.title.let { title ->
+                Text(
+                    title,
+                    fontSize = 12.sp,
+                    color = if (tabNavigator.current == tab) Color.Red else if (isDark) Color.White else Color.Black
+                )
+            }
+        },
+        enabled = true,
+        alwaysShowLabel = true,
+        interactionSource = MutableInteractionSource(),
+        colors = NavigationBarItemColors(
+            selectedTextColor = Color.Red,
+            selectedIconColor = Color.Red,
+            unselectedTextColor = Color.Black,
+            unselectedIconColor = Color.Black,
+            selectedIndicatorColor = Color.Gray,
+            disabledIconColor = Color.Black,
+            disabledTextColor = Color.Black
+        )
+    )
 }
 
 internal expect fun openUrl(url: String?)
