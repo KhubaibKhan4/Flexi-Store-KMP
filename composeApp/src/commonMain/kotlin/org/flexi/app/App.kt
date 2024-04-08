5eba297fd53066bc4f63b1d7dc0c30839caf4e82
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-08 15:43:39 +0500

    -Server Base Url Setup (#110)

--- composeApp/src/commonMain/kotlin/org/flexi/app/App.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/App.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -117,7 +118,9 @@ fun AppContent() {
             }
         }) {
             Column(
-                modifier = Modifier.fillMaxSize().padding(
+                modifier = Modifier.fillMaxSize()
+                    .navigationBarsPadding()
+                    .padding(
                     top = it.calculateTopPadding(),
                     start = if (showNavigationRail) 80.dp else 0.dp
                 )
