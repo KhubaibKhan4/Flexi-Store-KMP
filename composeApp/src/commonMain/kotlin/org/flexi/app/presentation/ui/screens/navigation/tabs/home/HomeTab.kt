463595ed3b8f5b29f5034d7945a32d782477c0b9
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-08 12:05:42 +0500

    -HomeTab Impl (#90)

---
+++ composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/navigation/tabs/home/HomeTab.kt
@@ -0,0 +1,29 @@
+package org.flexi.app.presentation.ui.screens.navigation.tabs.home
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabOptions
+import org.flexi.app.presentation.ui.screens.home.HomeScreen
+
+object HomeTab: Tab {
+    @Composable
+    override fun Content() {
+       Navigator(HomeScreen())
+    }
+
+    override val options: TabOptions
+        @Composable
+        get() {
+            val title by remember { mutableStateOf("Home") }
+            val icon = rememberVectorPainter(Icons.Default.Home)
+            val index : UShort = 0u
+            return TabOptions(index,title,icon)
+        }
+}
