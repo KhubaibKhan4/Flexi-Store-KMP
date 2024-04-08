7611212a4a39a16f9254c0053f8e52e60a5887a0
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-08 12:00:00 +0500

    -Tab Voyagar Impl (#89)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -64,6 +64,8 @@ kotlin {
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.voyager.navigator)
+            implementation(libs.voyager.tab.navigator)
+            implementation(libs.voyager.transitions)
             implementation(libs.composeImageLoader)
             implementation(libs.napier)
             implementation(libs.kotlinx.coroutines.core)
