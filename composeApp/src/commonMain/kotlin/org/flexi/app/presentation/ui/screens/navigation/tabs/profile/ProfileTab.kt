dcf2ec57a1a1aa93cfc59998f54c5875121391b3
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-08 12:15:37 +0500

    -Profile tab Impl (#110)

---
+++ composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/navigation/tabs/profile/ProfileTab.kt
@@ -0,0 +1,28 @@
+package org.flexi.app.presentation.ui.screens.navigation.tabs.profile
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PersonOutline
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabOptions
+
+object ProfileTab : Tab {
+    @Composable
+    override fun Content() {
+        Text("Profile Tab")
+    }
+
+    override val options: TabOptions
+        @Composable
+        get() {
+            val title by remember { mutableStateOf("Profile") }
+            val icon = rememberVectorPainter(Icons.Default.PersonOutline)
+            val index: UShort = 4u
+            return TabOptions(index, title, icon)
+        }
+}
