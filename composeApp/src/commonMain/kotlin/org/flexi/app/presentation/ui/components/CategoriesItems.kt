54988884f06c9f8831f60ca9c336c32380ca3e91
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-14 09:49:10 +0500

    -Custom Font Impl (#163)

--- composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/components/CategoriesItems.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/components/CategoriesItems.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -22,15 +21,20 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import flexi_store.composeapp.generated.resources.Res
+import flexi_store.composeapp.generated.resources.Roboto_Bold
 import io.kamel.core.Resource
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
 import org.flexi.app.domain.model.category.Categories
 import org.flexi.app.utils.Constant.BASE_URL
+import org.jetbrains.compose.resources.Font
+import org.jetbrains.compose.resources.FontResource
 
 @Composable
 fun CategoriesList(categories: List<Categories>) {
@@ -87,6 +91,9 @@ fun CategoriesItems(
                     text = categories.name,
                     fontSize = 34.sp,
                     fontWeight = FontWeight.ExtraBold,
+                    fontFamily = FontFamily(
+                        Font(resource = Res.font.Roboto_Bold)
+                    ),
                     color = Color.White,
                     textAlign = TextAlign.Center
                 )
