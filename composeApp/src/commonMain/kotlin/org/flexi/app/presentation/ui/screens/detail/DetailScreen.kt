6dca778e6105e0611dfb84d45af1777e776b1cc8
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-14 12:34:31 +0500

    -Product Details Card Impl (#175)

--- composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/detail/DetailScreen.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/detail/DetailScreen.kt
@@ -6,14 +6,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.ShoppingBasket
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -25,6 +27,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
@@ -46,55 +49,75 @@ class DetailScreen(
         val navigator = LocalNavigator.current
         var producstItems by remember { mutableStateOf(0) }
 
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Top
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.TopCenter
         ) {
-            Box(
+            Column(
                 modifier = Modifier.fillMaxWidth(),
-                contentAlignment = Alignment.TopCenter
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween
             ) {
-                val image: Resource<Painter> =
-                    asyncPainterResource(data = BASE_URL + products.imageUrl)
-                KamelImage(
-                    resource = image,
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxWidth()
-                        .height(350.dp),
-                    contentScale = ContentScale.Crop,
-                    onLoading = {
-                        CircularProgressIndicator(progress = { it })
-                    },
-                    onFailure = {
-                        Text(it.toString())
-                    }
-                )
-                Row(
-                    modifier = Modifier.fillMaxWidth()
-                        .background(color = Color.Black.copy(alpha = 0.05f))
-                        .padding(horizontal = 8.dp, vertical = 8.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.Top
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.TopCenter
                 ) {
-                    Icon(
-                        imageVector = Icons.Default.ArrowBackIosNew,
-                        contentDescription = "Arrow Back",
-                        modifier = Modifier.size(25.dp)
-                            .clickable { navigator?.pop() }
+                    val image: Resource<Painter> =
+                        asyncPainterResource(data = BASE_URL + products.imageUrl)
+                    KamelImage(
+                        resource = image,
+                        contentDescription = null,
+                        modifier = Modifier.fillMaxWidth()
+                            .height(350.dp),
+                        contentScale = ContentScale.Crop,
+                        onLoading = {
+                            CircularProgressIndicator(progress = { it })
+                        },
+                        onFailure = {
+                            Text(it.toString())
+                        }
                     )
-                    Text(
-                        text = "Detail Product",
-                        fontSize = MaterialTheme.typography.titleMedium.fontSize,
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.align(Alignment.CenterVertically)
-                    )
-                    Icon(
-                        imageVector = Icons.Default.ShoppingBasket,
-                        contentDescription = "Shopping Cart",
-                        modifier = Modifier.size(25.dp)
-                    )
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                            .background(color = Color.Black.copy(alpha = 0.05f))
+                            .padding(horizontal = 8.dp, vertical = 8.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.Top
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBackIosNew,
+                            contentDescription = "Arrow Back",
+                            modifier = Modifier.size(25.dp)
+                                .clickable { navigator?.pop() }
+                        )
+                        Text(
+                            text = "Detail Product",
+                            fontSize = MaterialTheme.typography.titleMedium.fontSize,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.align(Alignment.CenterVertically)
+                        )
+                        Icon(
+                            imageVector = Icons.Default.ShoppingBasket,
+                            contentDescription = "Shopping Cart",
+                            modifier = Modifier.size(25.dp)
+                        )
+                    }
                 }
+            }
+            Card(
+                modifier = Modifier.fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+                    .clip(
+                        RoundedCornerShape(topStartPercent = 24, topEndPercent = 24)
+                    ),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.White
+                )
+            ) {
+                Text(
+                    products.name,
+                    fontSize = MaterialTheme.typography.titleMedium.fontSize
+                )
             }
         }
     }
