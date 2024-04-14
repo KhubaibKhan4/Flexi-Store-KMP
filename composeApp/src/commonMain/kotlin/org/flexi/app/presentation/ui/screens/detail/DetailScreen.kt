310fd9cb63626e02ade20f38e8820657839fad41
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-14 11:30:00 +0500

    -Detail Screen TopBar Enhancement (#172)

--- composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/detail/DetailScreen.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/detail/DetailScreen.kt
@@ -1,25 +1,17 @@
 package org.flexi.app.presentation.ui.screens.detail
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.ShoppingCart
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -31,14 +23,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
 import io.kamel.core.Resource
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
@@ -50,6 +41,7 @@ class DetailScreen(
 ) : Screen {
     @Composable
     override fun Content() {
+        val navigator = LocalNavigator.current
         var producstItems by remember { mutableStateOf(0) }
         Column(
             modifier = Modifier.fillMaxWidth(),
@@ -80,26 +72,34 @@ class DetailScreen(
                     contentAlignment = Alignment.TopCenter
                 ) {
                     Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        verticalAlignment = Alignment.Top,
-                        horizontalArrangement = Arrangement.SpaceEvenly
+                        modifier = Modifier.fillMaxWidth()
+                            .padding(6.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
                     ) {
                         Icon(
                             imageVector = Icons.Default.ArrowBackIosNew,
-                            contentDescription = null
+                            contentDescription = null,
+                            tint = Color.White,
+                            modifier = Modifier.size(25.dp).clickable {
+                                navigator?.pop()
+                            }
                         )
                         Text(
                             text = "Detail Product",
-                            fontSize = MaterialTheme.typography.labelSmall.fontSize,
-                            fontWeight = FontWeight.SemiBold
+                            fontSize = MaterialTheme.typography.titleMedium.fontSize,
+                            fontWeight = FontWeight.SemiBold,
+                            color = Color.White
                         )
                         Icon(
                             imageVector = Icons.Default.ShoppingCart,
-                            contentDescription = "Shopping Cart"
+                            contentDescription = "Shopping Cart",
+                            tint = Color.White,
+                            modifier = Modifier.size(25.dp)
                         )
                     }
                 }
-                Card(
+                /*Card(
                     modifier = Modifier.fillMaxWidth(),
                     shape = RoundedCornerShape(14.dp),
                     colors = CardDefaults.cardColors(
@@ -205,7 +205,7 @@ class DetailScreen(
                             }
                         }
                     }
-                }
+                }*/
             }
         }
     }
