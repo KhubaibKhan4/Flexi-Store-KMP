1dee6370c22997e7b312030d6c564f507ba7fbf2
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-14 12:15:56 +0500

    -Custom TopApp Enhancement (#174)

--- composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/detail/DetailScreen.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/detail/DetailScreen.kt
@@ -1,12 +1,22 @@
 package org.flexi.app.presentation.ui.screens.detail
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material.icons.filled.ShoppingBasket
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,8 +25,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
@@ -58,6 +70,31 @@ class DetailScreen(
                         Text(it.toString())
                     }
                 )
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                        .background(color = Color.Black.copy(alpha = 0.05f))
+                        .padding(horizontal = 8.dp, vertical = 8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.Top
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBackIosNew,
+                        contentDescription = "Arrow Back",
+                        modifier = Modifier.size(25.dp)
+                            .clickable { navigator?.pop() }
+                    )
+                    Text(
+                        text = "Detail Product",
+                        fontSize = MaterialTheme.typography.titleMedium.fontSize,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                    )
+                    Icon(
+                        imageVector = Icons.Default.ShoppingBasket,
+                        contentDescription = "Shopping Cart",
+                        modifier = Modifier.size(25.dp)
+                    )
+                }
             }
         }
     }
