d5f2774a48d286c9a5a110d739b51203017419d0
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-17 16:41:24 +0500

    -Featured Product List Impl (#225)

---
+++ composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/components/ElectronicsItems.kt
@@ -0,0 +1,153 @@
+package org.flexi.app.presentation.ui.components
+
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.BaselineShift
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import flexi_store.composeapp.generated.resources.Res
+import flexi_store.composeapp.generated.resources.Roboto_Bold
+import io.kamel.core.Resource
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
+import org.flexi.app.domain.model.products.Products
+import org.flexi.app.utils.Constant.BASE_URL
+import org.jetbrains.compose.resources.Font
+
+@Composable
+fun FeaturedList(products: List<Products>) {
+    LazyRow {
+        items(products) { product ->
+            FeaturedItems(product)
+        }
+    }
+}
+
+@Composable
+fun FeaturedItems(
+    products: Products,
+) {
+    val image: Resource<Painter> = asyncPainterResource(BASE_URL + products.imageUrl)
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(16.dp),
+    ) {
+        Box(modifier = Modifier.fillMaxWidth()) {
+            KamelImage(
+                resource = image,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(180.dp)
+                    .clip(RoundedCornerShape(14.dp)),
+                contentScale = ContentScale.Crop,
+                animationSpec = tween(),
+                onLoading = {
+                    CircularProgressIndicator()
+                },
+                onFailure = {
+                    Text("Failed to Load")
+                }
+            )
+            Icon(
+                imageVector = Icons.Outlined.Favorite,
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(top = 6.dp, start = 8.dp)
+                    .size(25.dp)
+                    .clip(CircleShape)
+                    .background(
+                        Color.DarkGray.copy(alpha = 0.65f)
+                    )
+                    .align(Alignment.TopStart)
+                    .padding(4.dp)
+            )
+        }
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = products.name,
+                fontSize = MaterialTheme.typography.titleMedium.fontSize,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                maxLines = 1,
+                fontFamily = FontFamily(Font(Res.font.Roboto_Bold)),
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.fillMaxWidth()
+                    .padding(start = 3.dp, end = 3.dp)
+            )
+            Text(
+                text = products.categoryTitle,
+                fontSize = MaterialTheme.typography.bodySmall.fontSize,
+                fontWeight = FontWeight.Normal,
+                color = Color(0xFFe85110),
+                textAlign = TextAlign.Center,
+                fontFamily = FontFamily(Font(Res.font.Roboto_Bold)),
+            )
+            val price = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color(0xFFe85110),
+                        fontSize = 13.sp,
+                        fontWeight = FontWeight.Bold,
+                        baselineShift = BaselineShift.Superscript
+                    )
+                ) {
+                    append("\$")
+                }
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Black,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                ) {
+                    append(products.price.toString() + ".00")
+                }
+            }
+            Text(
+                text = price,
+                modifier = Modifier.padding(start = 10.dp),
+                fontSize = MaterialTheme.typography.titleMedium.fontSize,
+                fontWeight = FontWeight.SemiBold,
+                fontFamily = FontFamily(Font(Res.font.Roboto_Bold)),
+            )
+        }
+    }
+}
