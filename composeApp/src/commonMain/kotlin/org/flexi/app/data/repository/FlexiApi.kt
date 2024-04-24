1cea175f9b543556197a6f67ae6facac6e410a26
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-24 12:34:14 +0500

    -get Cart List FlexiApi Impl (#288)

--- composeApp/src/commonMain/kotlin/org/flexi/app/data/repository/FlexiApi.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/data/repository/FlexiApi.kt
@@ -1,6 +1,7 @@
 package org.flexi.app.data.repository
 
 import org.flexi.app.domain.model.books.BooksItem
+import org.flexi.app.domain.model.cart.CartItem
 import org.flexi.app.domain.model.category.Categories
 import org.flexi.app.domain.model.products.Products
 import org.flexi.app.domain.model.promotions.PromotionsProductsItem
@@ -14,4 +15,5 @@ interface FlexiApi {
     suspend fun getPromotionsProducts(): List<PromotionsProductsItem>
     suspend fun getCategories(): List<Categories>
     suspend fun getBooksList(): List<BooksItem>
+    suspend fun getCartListByUserId(id: Long): List<CartItem>
 }
