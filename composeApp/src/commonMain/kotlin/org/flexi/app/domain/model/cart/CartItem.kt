d570a369a3bd1a5adf52b39e17fb00716041f1b9
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-24 12:29:03 +0500

    -Cart Items Model Impl (#286)

---
+++ composeApp/src/commonMain/kotlin/org/flexi/app/domain/model/cart/CartItem.kt
@@ -0,0 +1,15 @@
+package org.flexi.app.domain.model.cart
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CartItem(
+    @SerialName("productId")
+    val productId: Int = 0,
+    @SerialName("quantity")
+    val quantity: Int = 0,
+    @SerialName("userId")
+    val userId: Int = 0
+)
