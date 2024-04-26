d052cd21d5fbed0172b06446a9facd188fd2a13b
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-04-26 12:11:24 +0500

    -Address UI Impl (#326)

--- composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/components/AddressDetails.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/components/AddressDetails.kt
@@ -1,42 +1,117 @@
 package org.flexi.app.presentation.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import flexi_store.composeapp.generated.resources.Res
+import flexi_store.composeapp.generated.resources.maps
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
-fun AddressDetails(cityName: String, postalCode: String, address: String) {
-    Column(
+fun AddressDetails(
+    cityName: String,
+    postalCode: String,
+    address: String,
+    onEditClicked: () -> Unit
+) {
+    Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 8.dp)
+            .padding(vertical = 8.dp, horizontal = 8.dp),
+        elevation =CardDefaults.cardElevation( 4.dp),
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(Color.White)
     ) {
-        Text(
-            text = cityName,
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black
-        )
-        Spacer(modifier = Modifier.height(4.dp))
-        Text(
-            text = "Postal Code: $postalCode",
-            fontSize = 14.sp,
-            color = Color.DarkGray
-        )
-        Spacer(modifier = Modifier.height(4.dp))
-        Text(
-            text = address,
-            fontSize = 16.sp,
-            color = Color.DarkGray
-        )
+        Column(
+            modifier = Modifier
+                .padding(vertical = 12.dp, horizontal = 16.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Address",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+                Text(
+                    text = "Edit",
+                    fontSize = 16.sp,
+                    color = Color(0xFFe85110),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.clickable(onClick = onEditClicked)
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp, horizontal = 8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Spacer(modifier = Modifier.width(4.dp))
+                Image(
+                    painter = painterResource(Res.drawable.maps),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier
+                        .width(140.dp)
+                        .height(140.dp)
+                        .clip(RoundedCornerShape(14.dp))
+
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp)
+                ) {
+                    Text(
+                        text = cityName,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = "Postal Code: $postalCode",
+                        fontSize = 14.sp,
+                        color = Color.DarkGray
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = address,
+                        fontSize = 16.sp,
+                        color = Color.DarkGray
+                    )
+                }
+            }
+        }
     }
 }
--- composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/payment/PaymentScreen.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/payment/PaymentScreen.kt
@@ -1,13 +1,11 @@
 package org.flexi.app.presentation.ui.screens.payment
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
@@ -18,10 +16,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import org.flexi.app.presentation.ui.components.AddressDetails
@@ -45,6 +41,7 @@ class PaymentScreen : Screen {
                             navigator?.pop()
                         }
                     )
+                    Spacer(Modifier.weight(1f))
                     Text(
                         text = "My Cart",
                         fontSize = MaterialTheme.typography.titleMedium.fontSize,
@@ -64,37 +61,15 @@ class PaymentScreen : Screen {
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(vertical = 8.dp)
-                        .background(color = Color.White)
                         .padding(horizontal = 0.dp, vertical = 8.dp),
                     horizontalAlignment = Alignment.Start,
                     verticalArrangement = Arrangement.Top
                 ) {
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(
-                            text = "Address",
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Color.Black,
-                            modifier = Modifier.padding(start = 4.dp)
-                        )
-                        Text(
-                            text = "Edit",
-                            fontSize = 16.sp,
-                            color = Color(0xFFe85110),
-                            fontWeight = FontWeight.Bold,
-                            modifier = Modifier
-                                .padding(end = 4.dp)
-                        )
-                    }
-                    Spacer(modifier = Modifier.height(8.dp))
                     AddressDetails(
                         cityName = "New York",
                         postalCode = "10001",
-                        address = "1234 Elm Street, Apt 5A"
+                        address = "1234 Elm Street, Apt 5A",
+                        onEditClicked = {}
                     )
                 }
 
