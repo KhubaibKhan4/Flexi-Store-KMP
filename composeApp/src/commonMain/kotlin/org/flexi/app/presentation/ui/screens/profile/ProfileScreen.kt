3a1d2fff4e5f7eba9ad8a8e57323243d72f080e7
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-01 16:56:34 +0500

    -Profile UI Enhancemenet impl (#384)

--- composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/profile/ProfileScreen.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/profile/ProfileScreen.kt
@@ -1,15 +1,25 @@
 package org.flexi.app.presentation.ui.screens.profile
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Camera
 import androidx.compose.material.icons.outlined.MoreVert
-import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.PhotoCamera
+import androidx.compose.material.icons.outlined.PhotoCameraBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -17,11 +27,16 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
+import flexi_store.composeapp.generated.resources.Res
+import flexi_store.composeapp.generated.resources.avatar
+import org.jetbrains.compose.resources.painterResource
 
-class ProfileScreen: Screen {
+class ProfileScreen : Screen {
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     override fun Content() {
@@ -54,9 +69,51 @@ class ProfileScreen: Screen {
                         end = 4.dp
                     ),
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                verticalArrangement = Arrangement.Top
             ) {
+                Box(
+                    modifier = Modifier.wrapContentWidth()
+                        .padding(top = 10.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Image(
+                        painter = painterResource(Res.drawable.avatar),
+                        contentDescription = null,
+                        modifier = Modifier.size(150.dp)
+                            .clip(CircleShape)
+                    )
+                    Box(
+                        modifier = Modifier.size(25.dp)
+                            .align(Alignment.BottomEnd)
+                            .offset(x = (-23).dp, y = (-6).dp)
+                            .background(color = Color.LightGray, shape = CircleShape)
+                    ) {
+                        Image(
+                            imageVector = Icons.Outlined.PhotoCamera,
+                            contentDescription = null,
+                            modifier = Modifier.padding(4.dp)
+                        )
+                    }
 
+                }
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Top
+                ) {
+                    Text(
+                        text = "Pablo Valdes",
+                        fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "@am_pablo",
+                        fontSize = MaterialTheme.typography.titleSmall.fontSize,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
             }
         }
     }
