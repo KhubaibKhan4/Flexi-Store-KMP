3ef6729f645adbe90bacd9769f595f57ac690b4c
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-02 12:16:09 +0500

    -Home SearchBar Impl (#398)

--- composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/home/HomeScreen.kt
@@ -12,6 +12,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -58,7 +62,9 @@ import org.flexi.app.presentation.ui.components.ErrorBox
 import org.flexi.app.presentation.ui.components.FeaturedList
 import org.flexi.app.presentation.ui.components.FoodList
 import org.flexi.app.presentation.ui.components.FurnituresList
+import org.flexi.app.presentation.ui.components.HotSaleItem
 import org.flexi.app.presentation.ui.components.LoadingBox
+import org.flexi.app.presentation.ui.components.ProductItem
 import org.flexi.app.presentation.ui.components.ProductList
 import org.flexi.app.presentation.ui.components.PromotionCardWithPager
 import org.flexi.app.presentation.ui.components.TopAppBarWithProfile
@@ -169,6 +175,11 @@ class HomeScreen : Screen {
             }
         }
         var isDark by LocalThemeIsDark.current
+        val isSearching =query.isNotBlank()
+
+        val filteredProducts = productsList?.filter {
+            it.name.contains(query, ignoreCase = true)
+        }
         Scaffold(
             modifier = Modifier.fillMaxWidth(),
             topBar = {
@@ -256,64 +267,82 @@ class HomeScreen : Screen {
                         )
                     }
                 }
-                promoList?.let { promotion ->
-                    PromotionCardWithPager(promotion)
-                }
-                productsList?.let { pro ->
-                    FeaturedList(pro)
-                    FoodList(pro)
-                    FurnituresList(pro)
-                    ElectronicsList(pro)
-                }
-                booksList?.let { bookList ->
-                    BooksList(bookList)
-                }
-                categoriesList?.let { categories ->
-                    Column(
-                        modifier = Modifier.fillMaxWidth(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.Start
+                if (isSearching){
+                    val lazyState = rememberLazyGridState()
+                    LazyVerticalGrid(
+                        columns = GridCells.Adaptive(150.dp),
+                        state = lazyState,
+                        modifier = Modifier.fillMaxWidth()
+                            .height(1200.dp).padding(bottom = 34.dp, top = 10.dp),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp)
                     ) {
-                        Text(
-                            text = "Categories",
-                            fontWeight = FontWeight.ExtraBold,
-                            modifier = Modifier.padding(vertical = 8.dp),
-                            textAlign = TextAlign.Start,
-                            fontSize = 28.sp
-                        )
-                        Spacer(modifier = Modifier.height(6.dp))
-                        LazyRow {
-                            item {
-                                Text(
-                                    text = "All",
-                                    fontWeight = if (selectedCategoryIndex == 0) FontWeight.Bold else FontWeight.Normal,
-                                    textDecoration = if (selectedCategoryIndex == 0) TextDecoration.Underline else TextDecoration.None,
-                                    color = if (selectedCategoryIndex == 0) Color.Black else Color.Gray,
-                                    modifier = Modifier.clickable { selectedCategoryIndex = 0 }
-                                        .padding(end = 16.dp)
-                                )
-                            }
-                            itemsIndexed(categories) { index, category ->
-                                Text(
-                                    text = category.name,
-                                    fontWeight = if (selectedCategoryIndex == index + 1) FontWeight.Bold else FontWeight.Normal,
-                                    textDecoration = if (selectedCategoryIndex == index + 1) TextDecoration.Underline else TextDecoration.None,
-                                    color = if (selectedCategoryIndex == index + 1) Color.Black else Color.Gray,
-                                    modifier = Modifier.clickable {
-                                        selectedCategoryIndex = index + 1
-                                    }
-                                        .padding(end = 16.dp)
-                                )
+                        filteredProducts?.let{proList->
+                            items(proList) { pro ->
+                                HotSaleItem(pro)
                             }
                         }
+                    }
+                }else{
+                    promoList?.let { promotion ->
+                        PromotionCardWithPager(promotion)
+                    }
+                    productsList?.let { pro ->
+                        FeaturedList(pro)
+                        FoodList(pro)
+                        FurnituresList(pro)
+                        ElectronicsList(pro)
+                    }
+                    booksList?.let { bookList ->
+                        BooksList(bookList)
+                    }
+                    categoriesList?.let { categories ->
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.Start
+                        ) {
+                            Text(
+                                text = "Categories",
+                                fontWeight = FontWeight.ExtraBold,
+                                modifier = Modifier.padding(vertical = 8.dp),
+                                textAlign = TextAlign.Start,
+                                fontSize = 28.sp
+                            )
+                            Spacer(modifier = Modifier.height(6.dp))
+                            LazyRow {
+                                item {
+                                    Text(
+                                        text = "All",
+                                        fontWeight = if (selectedCategoryIndex == 0) FontWeight.Bold else FontWeight.Normal,
+                                        textDecoration = if (selectedCategoryIndex == 0) TextDecoration.Underline else TextDecoration.None,
+                                        color = if (selectedCategoryIndex == 0) Color.Black else Color.Gray,
+                                        modifier = Modifier.clickable { selectedCategoryIndex = 0 }
+                                            .padding(end = 16.dp)
+                                    )
+                                }
+                                itemsIndexed(categories) { index, category ->
+                                    Text(
+                                        text = category.name,
+                                        fontWeight = if (selectedCategoryIndex == index + 1) FontWeight.Bold else FontWeight.Normal,
+                                        textDecoration = if (selectedCategoryIndex == index + 1) TextDecoration.Underline else TextDecoration.None,
+                                        color = if (selectedCategoryIndex == index + 1) Color.Black else Color.Gray,
+                                        modifier = Modifier.clickable {
+                                            selectedCategoryIndex = index + 1
+                                        }
+                                            .padding(end = 16.dp)
+                                    )
+                                }
+                            }
 
-                        val filteredProducts = if (selectedCategoryIndex == 0) {
-                            productsList
-                        } else {
-                            productsList?.filter { it.categoryTitle == categories[selectedCategoryIndex - 1].name }
-                        }
-                        filteredProducts?.let { productList ->
-                            ProductList(products = productList)
+                            val filteredProducts = if (selectedCategoryIndex == 0) {
+                                productsList
+                            } else {
+                                productsList?.filter { it.categoryTitle == categories[selectedCategoryIndex - 1].name }
+                            }
+                            filteredProducts?.let { productList ->
+                                ProductList(products = productList)
+                            }
                         }
                     }
                 }
