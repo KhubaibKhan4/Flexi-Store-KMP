a30a8cc79bdfdb3800e116b93ad6b61c90b31667
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-04 18:27:03 +0500

    -Pull to refresh Impl (#452)

--- composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/home/HomeScreen.kt
@@ -3,6 +3,7 @@ package org.flexi.app.presentation.ui.screens.home
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -20,9 +21,13 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.FilterAlt
 import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -37,6 +42,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -50,6 +56,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.flexi.app.domain.model.books.BooksItem
 import org.flexi.app.domain.model.cart.CartItem
 import org.flexi.app.domain.model.category.Categories
@@ -64,18 +72,19 @@ import org.flexi.app.presentation.ui.components.FoodList
 import org.flexi.app.presentation.ui.components.FurnituresList
 import org.flexi.app.presentation.ui.components.HotSaleItem
 import org.flexi.app.presentation.ui.components.LoadingBox
-import org.flexi.app.presentation.ui.components.ProductItem
 import org.flexi.app.presentation.ui.components.ProductList
 import org.flexi.app.presentation.ui.components.PromotionCardWithPager
 import org.flexi.app.presentation.ui.components.TopAppBarWithProfile
-import org.flexi.app.presentation.ui.screens.auth.login.LoginScreen
 import org.flexi.app.presentation.ui.screens.cart.CartList
 import org.flexi.app.presentation.viewmodels.MainViewModel
 import org.flexi.app.theme.LocalThemeIsDark
 import org.koin.compose.koinInject
 
 class HomeScreen : Screen {
-    @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
+    @OptIn(
+        ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class,
+        ExperimentalFoundationApi::class
+    )
     @Composable
     override fun Content() {
         var productsList by remember { mutableStateOf<List<Products>?>(null) }
@@ -176,11 +185,29 @@ class HomeScreen : Screen {
             }
         }
         var isDark by LocalThemeIsDark.current
-        val isSearching =query.isNotBlank()
+        val isSearching = query.isNotBlank()
 
         val filteredProducts = productsList?.filter {
             it.name.contains(query, ignoreCase = true)
         }
+
+        val refreshScope = rememberCoroutineScope()
+        var refreshing by remember { mutableStateOf(false) }
+
+        fun refresh() {
+            refreshScope.launch {
+                delay(1500)
+                viewModel.getProducts()
+                viewModel.getPromotionsItems()
+                viewModel.getCategoriesList()
+                viewModel.getBooksList()
+                viewModel.getCartsList(1)
+                refreshing = false
+            }
+        }
+
+        val refreshState = rememberPullRefreshState(refreshing, ::refresh)
+
         Scaffold(
             modifier = Modifier.fillMaxWidth(),
             topBar = {
@@ -201,154 +228,168 @@ class HomeScreen : Screen {
                 }
             }
         ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(2000.dp)
-                    .padding(
-                        top = it.calculateTopPadding(),
-                        bottom = it.calculateBottomPadding(),
-                        start = 6.dp,
-                        end = 6.dp
-                    )
-                    .verticalScroll(scrollState),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(4.dp)
+            Box(
+                Modifier
+                    .padding(top = 40.dp)
+                    .pullRefresh(refreshState),
             ) {
-                Row(
+                Column(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(horizontal = 16.dp, vertical = 8.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
+                        .height(2000.dp)
+                        .padding(
+                            top = it.calculateTopPadding(),
+                            bottom = it.calculateBottomPadding(),
+                            start = 6.dp,
+                            end = 6.dp
+                        )
+                        .verticalScroll(scrollState),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(4.dp)
                 ) {
-                    TextField(
-                        value = query,
-                        onValueChange = { query = it },
+                    Row(
                         modifier = Modifier
-                            .weight(1f)
-                            .height(52.dp),
-                        shape = RoundedCornerShape(16.dp),
-                        placeholder = {
-                            Text(
-                                "Search your Product",
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp, vertical = 8.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        TextField(
+                            value = query,
+                            onValueChange = { query = it },
+                            modifier = Modifier
+                                .weight(1f)
+                                .height(52.dp),
+                            shape = RoundedCornerShape(16.dp),
+                            placeholder = {
+                                Text(
+                                    "Search your Product",
+                                    fontSize = 14.sp
+                                )
+                            },
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Outlined.Search,
+                                    contentDescription = "Search",
+                                    tint = Color.Gray
+                                )
+                            },
+                            colors = TextFieldDefaults.textFieldColors(
+                                cursorColor = Color.Black,
+                                focusedIndicatorColor = Color.Transparent,
+                                unfocusedIndicatorColor = Color.Transparent
+                            ),
+                            textStyle = TextStyle(
                                 fontSize = 14.sp
                             )
-                        },
-                        leadingIcon = {
-                            Icon(
-                                imageVector = Icons.Outlined.Search,
-                                contentDescription = "Search",
-                                tint = Color.Gray
-                            )
-                        },
-                        colors = TextFieldDefaults.textFieldColors(
-                            cursorColor = Color.Black,
-                            focusedIndicatorColor = Color.Transparent,
-                            unfocusedIndicatorColor = Color.Transparent
-                        ),
-                        textStyle = TextStyle(
-                            fontSize = 14.sp
                         )
-                    )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Card(
-                        modifier = Modifier
-                            .size(48.dp)
-                            .clip(RoundedCornerShape(16.dp)),
-                        elevation = CardDefaults.cardElevation(8.dp)
-                    ) {
-                        Icon(
-                            imageVector = Icons.Outlined.FilterAlt,
-                            contentDescription = "Filter",
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Card(
                             modifier = Modifier
-                                .clickable {
-                                    isDark = !isDark
-                                }
-                                .padding(12.dp),
-                            tint = Color.Gray,
-                        )
-                    }
-                }
-                if (isSearching){
-                    val lazyState = rememberLazyGridState()
-                    LazyVerticalGrid(
-                        columns = GridCells.Adaptive(150.dp),
-                        state = lazyState,
-                        modifier = Modifier.fillMaxWidth()
-                            .height(1200.dp).padding(bottom = 34.dp, top = 10.dp),
-                        horizontalArrangement = Arrangement.spacedBy(8.dp),
-                        verticalArrangement = Arrangement.spacedBy(8.dp)
-                    ) {
-                        filteredProducts?.let{proList->
-                            items(proList) { pro ->
-                                HotSaleItem(pro)
-                            }
-                        }
-                    }
-                }else{
-                    promoList?.let { promotion ->
-                        PromotionCardWithPager(promotion)
-                    }
-                    productsList?.let { pro ->
-                        FeaturedList(pro)
-                        FoodList(pro)
-                        FurnituresList(pro)
-                        ElectronicsList(pro)
-                    }
-                    booksList?.let { bookList ->
-                        BooksList(bookList)
-                    }
-                    categoriesList?.let { categories ->
-                        Column(
-                            modifier = Modifier.fillMaxWidth(),
-                            verticalArrangement = Arrangement.Center,
-                            horizontalAlignment = Alignment.Start
+                                .size(48.dp)
+                                .clip(RoundedCornerShape(16.dp)),
+                            elevation = CardDefaults.cardElevation(8.dp)
                         ) {
-                            Text(
-                                text = "Categories",
-                                fontWeight = FontWeight.ExtraBold,
-                                modifier = Modifier.padding(vertical = 8.dp),
-                                textAlign = TextAlign.Start,
-                                fontSize = 28.sp
+                            Icon(
+                                imageVector = Icons.Outlined.FilterAlt,
+                                contentDescription = "Filter",
+                                modifier = Modifier
+                                    .clickable {
+                                        isDark = !isDark
+                                    }
+                                    .padding(12.dp),
+                                tint = Color.Gray,
                             )
-                            Spacer(modifier = Modifier.height(6.dp))
-                            LazyRow {
-                                item {
-                                    Text(
-                                        text = "All",
-                                        fontWeight = if (selectedCategoryIndex == 0) FontWeight.Bold else FontWeight.Normal,
-                                        textDecoration = if (selectedCategoryIndex == 0) TextDecoration.Underline else TextDecoration.None,
-                                        color = if (selectedCategoryIndex == 0) Color.Black else Color.Gray,
-                                        modifier = Modifier.clickable { selectedCategoryIndex = 0 }
-                                            .padding(end = 16.dp)
-                                    )
-                                }
-                                itemsIndexed(categories) { index, category ->
-                                    Text(
-                                        text = category.name,
-                                        fontWeight = if (selectedCategoryIndex == index + 1) FontWeight.Bold else FontWeight.Normal,
-                                        textDecoration = if (selectedCategoryIndex == index + 1) TextDecoration.Underline else TextDecoration.None,
-                                        color = if (selectedCategoryIndex == index + 1) Color.Black else Color.Gray,
-                                        modifier = Modifier.clickable {
-                                            selectedCategoryIndex = index + 1
-                                        }
-                                            .padding(end = 16.dp)
-                                    )
+                        }
+                    }
+                    if (isSearching) {
+                        val lazyState = rememberLazyGridState()
+                        LazyVerticalGrid(
+                            columns = GridCells.Adaptive(150.dp),
+                            state = lazyState,
+                            modifier = Modifier.fillMaxWidth()
+                                .height(1200.dp).padding(bottom = 34.dp, top = 10.dp),
+                            horizontalArrangement = Arrangement.spacedBy(8.dp),
+                            verticalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            filteredProducts?.let { proList ->
+                                items(proList) { pro ->
+                                    HotSaleItem(pro)
                                 }
                             }
+                        }
+                    } else {
+                        promoList?.let { promotion ->
+                            PromotionCardWithPager(promotion)
+                        }
+                        productsList?.let { pro ->
+                            FeaturedList(pro)
+                            FoodList(pro)
+                            FurnituresList(pro)
+                            ElectronicsList(pro)
+                        }
+                        booksList?.let { bookList ->
+                            BooksList(bookList)
+                        }
+                        categoriesList?.let { categories ->
+                            Column(
+                                modifier = Modifier.fillMaxWidth(),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.Start
+                            ) {
+                                Text(
+                                    text = "Categories",
+                                    fontWeight = FontWeight.ExtraBold,
+                                    modifier = Modifier.padding(vertical = 8.dp),
+                                    textAlign = TextAlign.Start,
+                                    fontSize = 28.sp
+                                )
+                                Spacer(modifier = Modifier.height(6.dp))
+                                LazyRow {
+                                    item {
+                                        Text(
+                                            text = "All",
+                                            fontWeight = if (selectedCategoryIndex == 0) FontWeight.Bold else FontWeight.Normal,
+                                            textDecoration = if (selectedCategoryIndex == 0) TextDecoration.Underline else TextDecoration.None,
+                                            color = if (selectedCategoryIndex == 0) Color.Black else Color.Gray,
+                                            modifier = Modifier.clickable {
+                                                selectedCategoryIndex = 0
+                                            }
+                                                .padding(end = 16.dp)
+                                        )
+                                    }
+                                    itemsIndexed(categories) { index, category ->
+                                        Text(
+                                            text = category.name,
+                                            fontWeight = if (selectedCategoryIndex == index + 1) FontWeight.Bold else FontWeight.Normal,
+                                            textDecoration = if (selectedCategoryIndex == index + 1) TextDecoration.Underline else TextDecoration.None,
+                                            color = if (selectedCategoryIndex == index + 1) Color.Black else Color.Gray,
+                                            modifier = Modifier.clickable {
+                                                selectedCategoryIndex = index + 1
+                                            }
+                                                .padding(end = 16.dp)
+                                        )
+                                    }
+                                }
 
-                            val filteredProducts = if (selectedCategoryIndex == 0) {
-                                productsList
-                            } else {
-                                productsList?.filter { it.categoryTitle == categories[selectedCategoryIndex - 1].name }
-                            }
-                            filteredProducts?.let { productList ->
-                                ProductList(products = productList)
+                                val filteredProducts = if (selectedCategoryIndex == 0) {
+                                    productsList
+                                } else {
+                                    productsList?.filter { it.categoryTitle == categories[selectedCategoryIndex - 1].name }
+                                }
+                                filteredProducts?.let { productList ->
+                                    ProductList(products = productList)
+                                }
                             }
                         }
                     }
                 }
+                PullRefreshIndicator(
+                    refreshing = refreshing,
+                    state = refreshState,
+                    modifier = Modifier
+                        .align(Alignment.TopCenter)
+                )
             }
         }
     }
