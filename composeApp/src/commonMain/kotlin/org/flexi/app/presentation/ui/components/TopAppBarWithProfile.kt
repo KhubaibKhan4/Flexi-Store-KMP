777c63f16096a16d2f8d0f17610d44b4f439e58d
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-06-11 12:38:35 +0500

    -Web App Enhancement (#530)

--- composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/components/TopAppBarWithProfile.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/components/TopAppBarWithProfile.kt
@@ -1,5 +1,6 @@
 package org.flexi.app.presentation.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
--- composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/flexi/app/presentation/ui/screens/home/HomeScreen.kt
@@ -1,6 +1,7 @@
 package org.flexi.app.presentation.ui.screens.home
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
